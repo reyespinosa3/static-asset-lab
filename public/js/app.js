2e41894fca5257585e6bc5b02e0ea8bfa0502d54
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-04-14 18:10:38 -0500

    added a few more images

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,35 @@
 console.log("Up and Running");
 
-
+const showAll = function() {
+  $('.sports').toggleClass("hidden");
+}
 const showOne = function() {
   $('#one').toggleClass("hidden");
 }
-
 const showTwo = function() {
   $('#two').toggleClass("hidden");
 }
-
 const showThree = function() {
   $('#three').toggleClass("hidden");
 }
-
+const showFour = function() {
+  $('#four').toggleClass("hidden")
+}
+const showFive = function() {
+  $('#five').toggleClass("hidden")
+}
+const showSix = function() {
+  $('#six').toggleClass("hidden")
+}
+const showSeven = function() {
+  $('#seven').toggleClass("hidden")
+}
+const showEight = function() {
+  $('#eight').toggleClass("hidden")
+}
+$('#all').on('click', function() {
+  showAll();
+});
 $('#soccerdog').on('click', function() {
   showOne();
 });
@@ -21,4 +38,19 @@ $('#dribbledog').on('click', function() {
 });
 $('#goaliedog').on('click', function() {
   showThree();
+});
+$('#footballdog').on('click', function() {
+  showFour();
+});
+$('#catchfootballdog').on('click', function() {
+  showFive();
+});
+$('#hockeydog').on('click', function() {
+  showSix();
+});
+$('#tennisdog').on('click', function() {
+  showSeven();
+});
+$('#basketballdog').on('click', function() {
+  showEight();
 });
